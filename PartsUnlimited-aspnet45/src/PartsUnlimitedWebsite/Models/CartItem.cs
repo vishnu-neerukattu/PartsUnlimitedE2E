3a799cd70db40924b41bcb8bd0e31d6959184143
vishnu Neerukattu <venkatvishnuvardhan@gmail.com>
2020-05-04 08:27:05 +0200

    added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
-    {
+    {// making my second change
         [Key]
         public int CartItemId { get; set; }
 
